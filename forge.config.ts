72e302187908d30b2ce86a4f1363d2cefe546d8c
aadilmallick <aadiltj.mallick@gmail.com>
2024-06-29 10:19:00 -0400

    add certificate for windows squirrel

--- forge.config.ts
+++ forge.config.ts
@@ -6,7 +6,7 @@ import { MakerRpm } from "@electron-forge/maker-rpm";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
-import { MakerAppX } from "@electron-forge/maker-appx";
+// import { MakerAppX } from "@electron-forge/maker-appx";
 import path from "path";
 import dotenv from "dotenv";
 
